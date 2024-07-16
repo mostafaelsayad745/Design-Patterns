0032d2a3cf27ff06d8d30cd6dce768263b268c84
Mostafa Elsayad <mostafaelsayad745@gmail.com>
2024-07-16 13:54:41 +0300

    strategy

--- Strategy/Implementation.cs
+++ Strategy/Implementation.cs
@@ -1,3 +1,70 @@
 ﻿namespace Strategy
 {
+	/// <summary>
+	/// strategy 
+	/// </summary>
+	public interface IExportService
+	{
+		void Export(Order order);
+	}
+
+	/// <summary>
+	/// concrete strategy
+	/// </summary>
+	public class JsonExportService : IExportService
+	{
+		public void Export(Order order)
+		{
+            Console.WriteLine($"Exporting {order.Name} to Json");
+        }
+	}
+
+	/// <summary>
+	/// concrete strategy
+	/// </summary>
+	public class XMLExportService : IExportService
+	{
+		public void Export(Order order)
+		{
+			Console.WriteLine($"Exporting {order.Name} to XML");
+		}
+	}
+
+	/// <summary>
+	/// concrete strategy
+	/// </summary>
+	public class CSVExportService : IExportService
+	{
+		public void Export(Order order)
+		{
+			Console.WriteLine($"Exporting {order.Name} to CSV");
+		}
+	}
+
+	/// <summary>
+	/// context
+	/// </summary>
+	public class Order
+	{
+		
+		public string Customer { get; set; }
+        public string Name { get; set; }
+        public int Amount { get; set; }
+        public string? Description { get; set; }
+
+        public Order(string customer, string name, int amount)
+		{ 
+			Customer = customer;
+			Name = name;
+			Amount = amount;
+		}
+
+		public void Export(IExportService exportService)
+		{
+			if (exportService == null) {
+				throw new ArgumentNullException(nameof(exportService));
+			}
+			exportService?.Export(this);
+		}
+	}
 }
--- Strategy/Program.cs
+++ Strategy/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.Title = "Strategy";
+﻿using Strategy;
+
+Console.Title = "Strategy";
+
+var order = new Order("Mostafa Ahmed", "Order 1", 1000);
+order.Export(new CSVExportService());
+order.Export(new JsonExportService());

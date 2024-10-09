a2445f71c97c525520b909e07b287dfd6f5791fd
Mostafa Elsayad <mostafaelsayad745@gmail.com>
2024-10-09 14:31:25 +0300

    TemplateMethodAdded

--- TemplateMethod/Implementation.cs
+++ TemplateMethod/Implementation.cs
@@ -1,3 +1,61 @@
 ﻿namespace TemplateMethod
-{ 
+{
+	/// <summary>
+	/// Abstract class
+	/// </summary>
+	public abstract class MailParser
+	{
+		public virtual void FindServer()
+		{
+			Console.WriteLine("Finding server...");
+		}
+
+		public abstract void AuthenticateToServer();
+
+		public string ParseHtmlMailBody(string identifier)
+		{
+			Console.WriteLine("parsing html mail body ....");
+			return $"Parsed mail body with identifier {identifier}";
+		}
+
+		/// <summary>
+		/// template method
+		/// </summary>
+		/// <param name="identifier"></param>
+		public string ParseMailBody(string identifier)
+		{
+			Console.WriteLine("parsing mail body (in template method)....");
+			FindServer();
+			AuthenticateToServer();
+			return ParseHtmlMailBody(identifier);
+		}
+	}
+
+	public class ExchangeMaiIParser : MailParser
+	{
+		public override void AuthenticateToServer()
+		{
+			Console.WriteLine("Authenticating to Exchange server...");
+		}
+	}
+
+	public class GmailMailParser : MailParser
+	{
+		public override void AuthenticateToServer()
+		{
+			Console.WriteLine("Authenticating to Gmail server...");
+		}
+	}
+
+	public class EudoraMailParser : MailParser
+	{
+		public override void FindServer()
+		{
+            Console.WriteLine("Finding Eudora server through a custom algorithm...");
+		}
+		public override void AuthenticateToServer()
+		{
+			Console.WriteLine("Authenticating to Eudora server...");
+		}
+	}
 }
--- TemplateMethod/Program.cs
+++ TemplateMethod/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.Title = "Template Method";
+﻿using TemplateMethod;
+
+Console.Title = "Template Method";
+
+var exchangeMaiIParser = new ExchangeMaiIParser();
+exchangeMaiIParser.ParseMailBody("123");
+Console.WriteLine();
+
+
+var gmailMailParser = new GmailMailParser();
+gmailMailParser.ParseMailBody("456");
+Console.WriteLine();
+
+
+var eudoraMailParser = new EudoraMailParser();
+eudoraMailParser.ParseMailBody("789");
+Console.WriteLine();

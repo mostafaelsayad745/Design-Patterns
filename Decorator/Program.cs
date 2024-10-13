af5b584221039dec9ac7cf6f95d713fc84554abf
Mostafa Elsayad <mostafaelsayad745@gmail.com>
2024-10-13 13:33:05 +0300

    DecoratorAdded

--- Decorator/Program.cs
+++ Decorator/Program.cs
@@ -1,1 +1,27 @@
-﻿Console.Title = "Decorator";
+﻿using Decorator;
+
+Console.Title = "Decorator";
+
+// instantiate mail services
+var cloudMailService = new CloudMailService();
+cloudMailService.SendMail("Hello from cloud mail service");
+
+var onPremiseMailService = new OnPremiseMailService();
+onPremiseMailService.SendMail("Hello from on-premise mail service");
+
+//add behavior to mail services
+
+var statisticsDecorator = new StatisticsDecorator(cloudMailService);
+statisticsDecorator.SendMail($"Hi there via {nameof(StatisticsDecorator)} wrapper");
+
+// add more behavior to mail services
+
+var messageDatabaseDecorator = new MessageDatabaseDecorator(onPremiseMailService);
+messageDatabaseDecorator.SendMail($"Hi there via {nameof(MessageDatabaseDecorator)} wrapper , message1");
+
+messageDatabaseDecorator.SendMail($"Hi there via {nameof(MessageDatabaseDecorator)} wrapper , message2");
+
+foreach (var decorator in messageDatabaseDecorator.SentMessages)
+{
+    Console.WriteLine($"Stored message : \"{decorator}\"");
+}
